fbd739448ec6c2fb8241bdd5653787a92c2c7302
Kathawach Satianpakiranakorn <mmload@hotmail.com>
2015-01-18 12:43:35 +0700

    Start Project

---
+++ Tutorial1 Opening a Window/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tutorial1_Opening_a_Window
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
