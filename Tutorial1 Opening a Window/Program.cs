073cd7f89c921a1aa57d6bbb28c3aa0efa44c11f
Kathawach Satianpakiranakorn <mmload@hotmail.com>
2015-01-18 15:28:59 +0700

    Complete Tutorial1 Opening a Window

--- Tutorial1 Opening a Window/Program.cs
+++ Tutorial1 Opening a Window/Program.cs
@@ -1,14 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Tao.FreeGlut;
+using OpenGL;
 
 namespace Tutorial1_Opening_a_Window
 {
     class Program
     {
+        private static int width = 1200, height = 720;
+
         static void Main(string[] args)
         {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("OpenGL Tutorial");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutMainLoop();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static void OnRenderFrame()
+        {
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Glut.glutSwapBuffers();
         }
     }
 }
